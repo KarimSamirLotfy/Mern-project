89b02cee3e0d42c4dce82fd664960aaf27d0864d
karim lotfy <karim.samir.lotfy@gmail.com>
2021-11-02 19:20:44 +0100

    Routing is done

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import  Mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js'
 console.log('here')
 console.log('feature 2 i guess')
 
 console.log('some improvemnts on feature 3 + nodemon is definetly wokring')
 
 console.log('kimo is here')
+
+const app= express()
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// connect to local DB make sure it is working 
+
+// send all posts the posts method
+app.use('/posts', postRoutes)
